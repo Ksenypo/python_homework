f1dbe4a89ca9ea47ca272b35935159c371d0b272
Ksenypo <83445761+Ksenypo@users.noreply.github.com>
2021-12-15 13:23:25 +0300

    Add files via upload
    описание обьектов: задание классов Rectangle, Square, Circle

---
+++ rectangle.py
@@ -0,0 +1,27 @@
+class Rectangle:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def get_area(self):
+        return self.a * self.b
+
+
+class Square:
+    def __init__(self, a):
+        self.a = a
+
+    def get_area_sq(self):
+        return self.a ** 2
+
+
+import math
+
+
+class Circle:
+    def __init__(self, r):
+        self.r = r
+
+    def get_circle_area(self):
+        return self.r ** 2 * math.pi
+
